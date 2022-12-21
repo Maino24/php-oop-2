5327036112e2e48242a04c56265b0123f18fdb9e
Gabriele Mainetti <mg02.gabriele@gmail.com>
2022-12-21 17:04:01 +0100

    category

--- classi/category.php
+++ classi/category.php
@@ -3,6 +3,15 @@
 class Category{
     public $name;
     public $icon;
+
+    public function __construct(
+        String $name,
+        String $icon
+    )
+    {
+        $this->name = $name;
+        $this->icon = $icon;
+    }
 }
 
 

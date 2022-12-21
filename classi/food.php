935c3351e4cb403f26b8490c8bbe0db592e735d9
Gabriele Mainetti <mg02.gabriele@gmail.com>
2022-12-21 17:16:22 +0100

    food

---
+++ classi/food.php
@@ -0,0 +1,26 @@
+<?php
+
+include_once __DIR__ . '/product.php';
+
+class Food extends Product{
+    public $weight;
+    public $ingredienti;
+
+
+    public function __construct(
+        String $image,
+        String $nome,
+        Float $prezzo,
+        Category $category,
+
+        String $weight,
+        String $ingredienti
+    )
+    {
+        parent::__construct($image,$nome,$prezzo,$category);
+        $this->weight = $weight;
+        $this->ingredienti = $ingredienti;
+    }
+}
+
+?>
